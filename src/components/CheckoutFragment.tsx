be0b5b5ffaa0ad41d63548fb2f20d0eaef249575
dipesh <dipesh.singh2012@gmail.com>
2026-09-10 05:17:49 +0000

    feat(proton): migrate imports and dependency to @dipeshsingh2012/proton

--- src/components/CheckoutFragment.tsx
+++ src/components/CheckoutFragment.tsx
@@ -14,7 +14,7 @@ import {
   ProtonButton,
   ProtonCard,
   ProtonStatusBadge,
-} from 'proton/react';
+} from '@dipeshsingh2012/proton/react';
 import { submitOrder } from '../api';
 import { CustomerInfo, OrderReceipt, ShippingAddress } from '../types';
 
